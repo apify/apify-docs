7743bd3fc5f1e6e7a16ca5de77435364fdefab15
Marek Trunkat <marek@trunkat.eu>
2019-08-02 16:01:15 +0200

    Fixes of travis

--- s3_sync.sh
+++ s3_sync.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-GIT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+GIT_BRANCH=$TRAVIS_BRANCH
+[ -z "$GIT_BRANCH" ] && GIT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 
 # For now let's have a master and develop version as we don't change docs often.
 # Develop will contain the latest non-master version pushed to GitHub.
