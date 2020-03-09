19e5a76f0b15a43116dc85972c386d88275bbe2e
Marek Trunkat <marek@trunkat.eu>
2020-03-09 14:02:17 +0100

    Path fixes in apiary docs

--- upload_to_apiary.sh
+++ upload_to_apiary.sh
@@ -14,7 +14,7 @@ else
     export API_NAME="apify2prod"
 fi
 
-apiary publish --api-name=${API_NAME} --path="../src/app/.apiary-docs/api-v2-reference.apib"
+apiary publish --api-name=${API_NAME} --path="./api_v2_reference.apib"
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Cannot upload to Apiary (did you install apiary tool by running 'sudo gem install apiaryio'?)"
