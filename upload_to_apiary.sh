e955e466ef17b95e691e70a38b04d4cd717f3c9b
Marek Trunkat <marek@trunkat.eu>
2020-03-09 13:56:30 +0100

    Adding deployment to Apiary

---
+++ upload_to_apiary.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+GIT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+
+# For now let's have a master and develop version as we don't change docs often.
+# Develop will contain the latest non-master version pushed to GitHub.
+if [ "$GIT_BRANCH" != "master" ]; then
+    echo "Deploying to Apiary as develop documentation..."
+    export API_NAME="apify2staging"
+else
+    echo "Deploying to Apiary as production documentation..."
+    export API_NAME="apify2prod"
+fi
+
+apiary publish --api-name=${API_NAME} --path="../src/app/.apiary-docs/api-v2-reference.apib"
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Cannot upload to Apiary (did you install apiary tool by running 'sudo gem install apiaryio'?)"
+    exit 1
+fi
