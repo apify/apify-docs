5fd49b07fc4630c346cb999d9cf151b01056e121
Marek Trunkat <marek@trunkat.eu>
2020-03-09 13:39:33 +0100

    Adding more info to log

--- s3_sync.sh
+++ s3_sync.sh
@@ -5,10 +5,15 @@ set -e
 GIT_BRANCH=$TRAVIS_BRANCH
 [ -z "$GIT_BRANCH" ] && GIT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 
+echo "Echo Git branch:"
+echo $GIT_BRANCH
+
 # For now let's have a master and develop version as we don't change docs often.
 # Develop will contain the latest non-master version pushed to GitHub.
 if [ "$GIT_BRANCH" != "master" ]; then
+    echo "Deploying as develop documentation..."
     aws s3 sync --delete --exclude master --acl public-read build s3://apify-docs/develop
 else
+    echo "Deploying as production documentation..."
     aws s3 sync --delete --exclude develop --acl public-read build s3://apify-docs/master
 fi
