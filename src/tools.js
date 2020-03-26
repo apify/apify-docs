5ebc905a081bca784d43da65a9236625d2a5a26b
Marek Trunkat <marek@trunkat.eu>
2020-03-26 16:29:55 +0100

    Linting code and adding linter to CI

--- src/tools.js
+++ src/tools.js
@@ -5,8 +5,6 @@ const { ensureDir } = require('fs-extra');
 
 const { BUILD_DIR_PATH, DOCS_PATH } = require('./consts');
 
-const existsPromised = util.promisify(fs.exists);
-const mkdirPromised = util.promisify(fs.mkdir);
 const writeFilePromised = util.promisify(fs.writeFile);
 const copyFilePromised = util.promisify(fs.copyFile);
 
