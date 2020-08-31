426cf709df87a8765044a9226c824ad434ed1f0a
Matej Hamas <matej.hamas@gmail.com>
2020-08-31 16:30:56 +0200

    Echo string

--- src/scripts/upload_to_s3.sh
+++ src/scripts/upload_to_s3.sh
@@ -5,6 +5,6 @@ set -e
 GIT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 S3_PREFIX="s3://apify-docs/${GIT_BRANCH}"
 
-echo "Deploying branch: \"${GIT_BRANCH}\" to \"${S3_PREFIX}\""
+echo "Uploading artifacts for branch \"${GIT_BRANCH}\" to \"${S3_PREFIX}\""
 
 aws s3 sync --delete --acl public-read build "${S3_PREFIX}"
