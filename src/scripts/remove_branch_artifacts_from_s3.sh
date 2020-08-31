deef4995554617bf9906272fd08723d2adf0b4d6
Matej Hamas <matej.hamas@gmail.com>
2020-08-31 15:48:43 +0200

    Trying to fix how to get current branch name when closing branch

--- src/scripts/remove_branch_artifacts_from_s3.sh
+++ src/scripts/remove_branch_artifacts_from_s3.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-GIT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+if [ $# -eq 0 ]; then
+    echo "No branch name supplied, using git CLI to get the current branch"
+    GIT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+else
+    GIT_BRANCH=$1
+fi
+
 S3_PREFIX="s3://apify-docs/${GIT_BRANCH}"
 
 echo "Removing \"${S3_PREFIX}\""
