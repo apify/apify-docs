abeb9bccaf432ac92e195e6ee5fb61f8227c2ef0
Misha <m.murasovs@gmail.com>
2020-10-27 15:03:36 +0100

    Configure linter and fix typos

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
     extends: [
-        "plugin:markdown/recommended",
-        "@apify"
+        'plugin:markdown/recommended',
+        '@apify',
     ],
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
     plugins: [
-       "@babel"
+        '@babel',
     ],
     parserOptions: {
-        configFile: ".babelrc"
+        configFile: '.babelrc',
     },
     env: {
         es6: true,
         node: true,
     },
     rules: {
-        "import/no-extraneous-dependencies": "off",
-        "no-unused-vars": "off",
-        "no-unused-expressions": "off",
-        "no-undef": "off",
-        "no-console": "off",
+        'import/no-extraneous-dependencies': 'off',
+        'no-unused-vars': 'off',
+        'no-unused-expressions': 'off',
+        'no-undef': 'off',
+        'no-console': 'off',
     },
 };
