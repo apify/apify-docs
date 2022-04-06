04c08163b8f1f57e37a946e8a097f24ed03078a7
Misha <m.murasovs@gmail.com>
2022-04-06 16:18:20 +0200

    Re-add --delete to s3 upload script (#332)

--- src/scripts/upload_to_s3.sh
+++ src/scripts/upload_to_s3.sh
@@ -11,5 +11,4 @@ echo "Uploading artifacts for branch \"${GIT_BRANCH}\" to \"${S3_PREFIX}\""
 # The --delete removes content in bucket that isn't in the repo, so this way we can have both there at the same time
 # until the web part gets deployed
 
-# aws s3 sync --delete --acl public-read build "${S3_PREFIX}"
-aws s3 sync --acl public-read build "${S3_PREFIX}"
+aws s3 sync --delete --acl public-read build "${S3_PREFIX}"
