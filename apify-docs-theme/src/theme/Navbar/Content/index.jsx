d49ed0e19b6927745441bd160dc60b30c38bdbe3
Jindřich Bär <jindrichbar@gmail.com>
2023-03-17 13:25:52 +0100

    fix: optional subtitle `to` link, academy sidebar (#551)
    As per @mtrunkat 's request, this PR slightly changes the academy index
    page and aligns the behavior with the other projects' instances.

--- apify-docs-theme/src/theme/Navbar/Content/index.jsx
+++ apify-docs-theme/src/theme/Navbar/Content/index.jsx
@@ -45,7 +45,7 @@ function SubNavbar() {
                 <div className="navbar__container">
                     <div className="navbar__items">
                         <div className="navbar__sub--title">
-                            <NavbarItem label={subNavbar.title} to="/" activeBaseRegex='(?!)' />
+                            <NavbarItem label={subNavbar.title} to={subNavbar.to ?? '/'} activeBaseRegex='(?!)' />
                         </div>
                         <NavbarItems items={subNavbar.items}/>
                     </div>
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,6 +26,7 @@ module.exports = {
                 subNavbar: {
                     title: 'Academy',
                     pathRegex: '/academy',
+                    to: '/academy',
                     items: [
                         {
                             label: 'Courses',
