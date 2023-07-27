9c62443998f4d773187d9130fdfe4a37b892e82c
Martin Adámek <banan23@gmail.com>
2023-07-27 15:23:49 +0200

    feat: improve `<title>` tags for docs instances and API reference

---
+++ src/theme/DocItem/Metadata/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { PageMetadata } from '@docusaurus/theme-common';
+import { useDoc } from '@docusaurus/theme-common/internal';
+
+export default function DocItemMetadata() {
+    const { metadata, frontMatter, assets } = useDoc();
+    let { title } = metadata;
+    const section = metadata.source.match(/@site\/sources\/(.*?)\//)?.[1];
+    const sections = {
+        academy: 'Academy',
+        platform: 'Platform',
+    };
+
+    if (section && sections[section]) {
+        title = `${metadata.title} | ${sections[section]}`;
+    }
+
+    return (
+        <PageMetadata
+            title={title}
+            description={metadata.description}
+            keywords={frontMatter.keywords}
+            image={assets.image ?? frontMatter.image}
+        />
+    );
+}
