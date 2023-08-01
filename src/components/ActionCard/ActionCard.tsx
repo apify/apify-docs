631951b027ed5c983be697d776f78f4014085eb4
Jan Bárta <jbarta.edu@gmail.com>
2023-08-01 18:07:52 +0200

    feat: Added PlainCard and ActionCard components - wrappers over shared tile component coming from shared component (ui-components package)

---
+++ src/components/ActionCard/ActionCard.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { HorizontalTile, theme } from '@apify-packages/ui-components';
+import styles from './styles.module.css';
+import { Text } from '../Text';
+import { Heading } from '../Heading';
+
+import ArrowRight20 from '../../pages/img/arrow-right-20.svg';
+
+interface ActionCardProps {
+    title: string;
+    description?: string;
+    to: string;
+    width?: string;
+}
+
+export default function ActionCard({ title, description, to, width }: ActionCardProps) {
+    const { siteConfig } = useDocusaurusContext();
+    const external = to.startsWith('http');
+
+    const Tile = <HorizontalTile
+        style={{ height: '100%' }}
+        content={
+            <div className={styles.actionCardContent}>
+                <div className={styles.actionCardContentHeader}>
+                    <Heading type='titleM'>{title}</Heading>
+                </div>
+                <div className={styles.cardContentDescription}>
+                    <Text align='left' color={theme.color.neutral.textSubtle}>{description}</Text>
+                </div>
+            </div>
+        }
+        isClickable
+        action={<ArrowRight20 size={20} strokeWidth={1.5} /> }
+    />;
+
+    if (external) {
+        return (
+            <Link to={new URL(to, siteConfig.url).href} style={{ width }}>
+                {Tile}
+            </Link>
+        );
+    }
+
+    return (
+        <a href={new URL(to, siteConfig.url).href} style={{ width }}>
+            {Tile}
+        </a>
+    );
+}
