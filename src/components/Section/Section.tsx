944e9a6cb1708324a92bd44358c6ec72f5200737
Jan Bárta <jbarta.edu@gmail.com>
2023-08-01 18:06:58 +0200

    feat: Section component styling updates

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import styles from './styles.module.css';
+import { Heading } from '../Heading';
+import { Text } from '../Text';
+import { theme } from '@apify-packages/ui-components';
 
 interface SectionProps {
     heading?: string;
+    description?: string;
     children: React.ReactNode | string;
 }
 
-export default function Section({ heading, children }: SectionProps) {
+export default function Section({ heading, description, children }: SectionProps) {
     return (
         <section className={styles.section}>
-            <div className="container">
-                {heading && <h2>{heading}</h2>}
-                {children}
-            </div>
+            {(heading || description) && <div className={styles.sectionHeader}>
+                {heading && <Heading type='titleXl'>{heading}</Heading>}
+                {description && <Text color={theme.color.neutral.textMuted}>{description}</Text>}
+            </div>}
+            {children}
         </section>
     );
 }
