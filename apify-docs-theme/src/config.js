b98a8d3c79812cbb00e6dc9fd489a0c5d60862d1
Jindřich Bär <jindrichbar@gmail.com>
2023-08-08 22:56:14 +0200

    feat: add @apify/docs-search-modal

--- apify-docs-theme/src/config.js
+++ apify-docs-theme/src/config.js
@@ -241,10 +241,8 @@ const themeConfig = ({
     },
     algolia: {
         appId: 'N8EOCSBQGH',
-        apiKey: 'b43e67a96ed18c7f63f5fd965906a96d', // search only (public) API key
-        indexName: 'apify_sdk',
-        // apiKey: 'e97714a64e2b4b8b8fe0b01cd8592870', // search only (public) API key
-        // indexName: 'crawler_dev_apify_sdk',
+        apiKey: 'e97714a64e2b4b8b8fe0b01cd8592870', // search only (public) API key
+        indexName: 'test_test_apify_sdk',
         algoliaOptions: {
             facetFilters: ['version:VERSION'],
         },
