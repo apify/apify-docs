6f9ae14ad7bf560ac6239b835c636b0ba50dcdb4
Jindřich Bär <jindrichbar@gmail.com>
2023-08-15 11:21:08 +0200

    feat: search respects versioning

--- apify-docs-theme/src/theme/SearchBar/index.js
+++ apify-docs-theme/src/theme/SearchBar/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { ApifySearch } from '@apify/docs-search-modal';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useLocation } from '@docusaurus/router';
 
 export default function SearchBar() {
     const { siteConfig } = useDocusaurusContext();
+    const location = useLocation();
+
+    const getVersion = useCallback(() => {
+        const match = location.pathname.match(/\/(\d+\.\d+|next)/);
+
+        return match ? match[1] : 'latest';
+    }, [location]);
+
     return (
         <ApifySearch
             algoliaAppId={siteConfig.themeConfig.algolia.appId}
             algoliaIndexName='test_test_apify_sdk'
             algoliaKey={siteConfig.themeConfig.algolia.apiKey}
+            filters={`version:${getVersion()}`}
         />
     );
 }
