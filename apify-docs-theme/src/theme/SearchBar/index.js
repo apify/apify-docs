9247c264c66492b72d1c3ba7c29c99952c158b8b
Jindřich Bär <jindrichbar@gmail.com>
2023-08-16 13:26:21 +0200

    feat: update @apify/docs-search-modal, pass navigator

--- apify-docs-theme/src/theme/SearchBar/index.js
+++ apify-docs-theme/src/theme/SearchBar/index.js
@@ -2,11 +2,52 @@ import React, { useCallback } from 'react';
 
 import { ApifySearch } from '@apify/docs-search-modal';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { useLocation } from '@docusaurus/router';
+import { useLocation, useHistory } from '@docusaurus/router';
+
+import RouterLink from '@docusaurus/Link';
+
+/**
+ * Tests whether the given href is pointing to the current docusaurus instance (so we can use the router link).
+ */
+function matchesCurrentInstance(href, baseUrl) {
+    if (baseUrl === '/') {
+        return href.startsWith('/academy') || href.startsWith('/platform');
+    }
+
+    return href.startsWith(baseUrl);
+}
+
+export function Link(props) {
+    props = { ...props };
+
+    if (props.href.startsWith('https://docs.apify.com')) {
+        props.href = props.href.substring('https://docs.apify.com'.length);
+    }
+
+    const { siteConfig } = useDocusaurusContext();
+
+    if (matchesCurrentInstance(props.href, siteConfig.baseUrl)) {
+        return <RouterLink {...props}>
+            {props.children}
+        </RouterLink>;
+    }
+
+    return <a {...props}>{props.children}</a>;
+}
 
 export default function SearchBar() {
     const { siteConfig } = useDocusaurusContext();
     const location = useLocation();
+    const history = useHistory();
+
+    const navigate = useCallback((href) => {
+        const shortHref = href.substring('https://docs.apify.com'.length);
+
+        if (matchesCurrentInstance(shortHref, siteConfig.baseUrl)) {
+            return history.push(shortHref);
+        }
+        return window.location.assign(href);
+    }, [history, siteConfig.baseUrl]);
 
     const getVersion = useCallback(() => {
         const match = location.pathname.match(/\/(\d+\.\d+|next)/);
@@ -15,11 +56,14 @@ export default function SearchBar() {
     }, [location]);
 
     return (
-        <ApifySearch
-            algoliaAppId={siteConfig.themeConfig.algolia.appId}
-            algoliaIndexName='test_test_apify_sdk'
-            algoliaKey={siteConfig.themeConfig.algolia.apiKey}
-            filters={`version:${getVersion()}`}
-        />
+        <>
+            <ApifySearch
+                algoliaAppId={siteConfig.themeConfig.algolia.appId}
+                algoliaIndexName='test_test_apify_sdk'
+                algoliaKey={siteConfig.themeConfig.algolia.apiKey}
+                filters={`version:${getVersion()}`}
+                navigate={navigate}
+            />
+        </>
     );
 }
