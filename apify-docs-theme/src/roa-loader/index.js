fb142b805013d8b3297e3b50db984d5f6b885ec8
Martin Adámek <banan23@gmail.com>
2023-09-21 16:06:02 +0200

    feat: add Run on Apify button support

---
+++ apify-docs-theme/src/roa-loader/index.js
@@ -0,0 +1,73 @@
+const { urlToRequest } = require('loader-utils');
+const { inspect } = require('util');
+
+const signingUrl = new URL('https://api.apify.com/v2/tools/encode-and-sign');
+signingUrl.searchParams.set('token', process.env.APIFY_SIGNING_TOKEN);
+const queue = [];
+let working = false;
+
+async function getHash(source) {
+    const memory = source.match(/playwright|puppeteer/i) ? 4096 : 1024;
+    const res = await (await fetch(signingUrl, {
+        method: 'POST',
+        body: JSON.stringify({
+            input: JSON.stringify({ code: source }),
+            options: {
+                build: 'latest',
+                contentType: 'application/json; charset=utf-8',
+                memory,
+                timeout: 180,
+            },
+        }),
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+    })).json();
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    if (!res.data || !res.data.encoded) {
+        // eslint-disable-next-line no-console
+        console.error(`Signing failed:' ${inspect(res.error) || 'Unknown error'}`, res);
+        return 'invalid-token';
+    }
+
+    return res.data.encoded;
+}
+
+async function encodeAndSign(source) {
+    if (working) {
+        return new Promise((resolve, reject) => {
+            queue.push(() => {
+                return getHash(source).then(resolve, reject);
+            });
+        });
+    }
+
+    let res;
+
+    try {
+        working = true;
+        res = await getHash(source);
+
+        while (queue.length) {
+            await queue.shift()();
+        }
+    } finally {
+        working = false;
+    }
+
+    return res;
+}
+
+module.exports = async function (code) {
+    if (process.env.CRAWLEE_DOCS_FAST) {
+        return { code, hash: 'fast' };
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(`Signing ${urlToRequest(this.resourcePath)}...`, { working, queue: queue.length });
+    const hash = await encodeAndSign(code);
+
+    return { code, hash };
+};
---
+++ sources/academy/tutorials/node_js/scraping_from_sitemaps.js
@@ -0,0 +1,28 @@
+import { Dataset, RequestList, PuppeteerCrawler } from 'crawlee';
+
+const requestList = await RequestList.open(null, [{
+    requestsFromUrl: 'https://www.brewbound.com/sitemap.xml',
+    regex: /http(s)?:\/\/www\.brewbound\.com\/breweries\/[^/<]+\/[^/<]+/gm,
+}]);
+
+const crawler = new PuppeteerCrawler({
+    requestList,
+    async requestHandler({ page }) {
+        const beerPage = await page.evaluate(() => {
+            return document.getElementsByClassName('productreviews').length;
+        });
+        if (!beerPage) return;
+
+        const data = await page.evaluate(() => {
+            const title = document.getElementsByTagName('h1')[0].innerText;
+            const [brewery, beer] = title.split(':');
+            const description = document.getElementsByClassName('productreviews')[0].innerText;
+
+            return { brewery, beer, description };
+        });
+
+        await Dataset.pushData(data);
+    },
+});
+
+await crawler.run();
