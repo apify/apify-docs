c83c7a2d07ea2c6431703a4f4f7f8358f076f7da
Vlad Frangu <kingdgrizzle@gmail.com>
2024-01-08 20:45:26 +0200

    fix: make docs work on windows

--- tools/utils/collectSlugs.js
+++ tools/utils/collectSlugs.js
@@ -1,8 +1,32 @@
-const { execSync } = require('child_process');
+const { opendirSync, readFileSync } = require('fs');
 
 function collectSlugs(pathname) {
-    const a = execSync(`find ${pathname} -type f -name "*.md" -exec grep slug: {} \\;`, { encoding: 'utf-8' });
-    return a.split('\n').filter((x) => x.startsWith('slug: ')).map((x) => x.replace('slug: ', ''));
+    const dir = opendirSync(pathname);
+    const res = [];
+
+    let direntry;
+
+    // eslint-disable-next-line no-cond-assign
+    while ((direntry = dir.readSync()) !== null) {
+        if (direntry.isFile() && direntry.name.endsWith('.md')) {
+            const mdContent = readFileSync(`${pathname}/${direntry.name}`, { encoding: 'utf-8' });
+
+            const slugMatch = mdContent.match(/^slug: (.*)$/m);
+            if (slugMatch) {
+                res.push(slugMatch[1]);
+            }
+        }
+
+        if (direntry.isDirectory()) {
+            const dirPath = `${pathname}/${direntry.name}`;
+            const dirRes = collectSlugs(dirPath);
+            res.push(...dirRes);
+        }
+    }
+
+    dir.closeSync();
+
+    return res;
 }
 
 module.exports = {
