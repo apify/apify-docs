d50520579fb2d4bdb14ce5f3d57320009a6b75cb
Martin Adámek <banan23@gmail.com>
2024-02-02 15:45:42 +0100

    docs: disable selection of API docs pills + handle absolute (but local) links from readme (#838)
    Closes #805

--- apify-docs-theme/src/theme/MDXComponents/A.js
+++ apify-docs-theme/src/theme/MDXComponents/A.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Link from '@docusaurus/Link';
 import { isDifferentInstance } from '../../utils';
 
 export default function MDXA(props) {
+    const { siteConfig } = useDocusaurusContext();
+
+    if (props.href?.startsWith(siteConfig.url) && isDifferentInstance(siteConfig.baseUrl)) {
+        const { href, ...rest } = props;
+        rest.to = props.href.replace(siteConfig.url + siteConfig.baseUrl, '/');
+        props = rest;
+    }
+
     if (props.href && isDifferentInstance(props.href)) {
         return <a {...props} onClick={((e) => {
             e.preventDefault();
