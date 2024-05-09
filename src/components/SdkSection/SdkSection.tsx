8cc90dbfa36a0123c79a974017cabb4c3dca1348
Eva Baldasseroni <37815707+baldasseva@users.noreply.github.com>
2024-05-09 16:53:34 +0200

    capitalize class names, use correct code snippet and other pr fixes

--- src/components/SdkSection/SdkSection.tsx
+++ src/components/SdkSection/SdkSection.tsx
@@ -39,10 +39,10 @@ export default function SdkSection({
     const lowerCaseLanguage = language.toLowerCase();
 
     return (
-        <div className={styles.sdkSection}>
-            <div className={styles.sdkSectionDescription}>
+        <div className={styles.SdkSection}>
+            <div className={styles.SdkSectionDescription}>
                 <div>
-                    <div className={styles.sdkSectionTitle}>
+                    <div className={styles.SdkSectionTitle}>
                         <ThemedImage
                             height={24}
                             width={24}
@@ -59,18 +59,18 @@ export default function SdkSection({
                     data-color-scheme={colorMode}
                     data-size="large"
                     data-show-count="true"
-                    aria-label={`Star apify/apify-sdk-${lowerCaseLanguage} on GitHub`}
+                    aria-label={`Star apify/apify-sdk-${lowerCaseLanguage === 'javascript' ? 'js' : lowerCaseLanguage} on GitHub`}
                 >
                     Star
                 </GitHubButton>
-                <div className={styles.sdkSectionActionButtons}>
+                <div className={styles.SdkSectionActionButtons}>
                     <StyledButton hideExternalIcon color='success' to={gettingStartedUrl}>Get started</StyledButton>
                     <ActionLink hideExternalIcon to={referenceUrl} >{`${language} SDK Reference`}</ActionLink>
                 </div>
             </div>
-            <div className={styles.sdkSectionCodeExamples}>
+            <div className={styles.SdkSectionCodeExamples}>
                 <CodeBlock language="bash" content={installCodeSnippet} />
-                <CodeBlock language={lowerCaseLanguage} hideLineNumbers content={exampleCodeSnippet} />
+                <CodeBlock language={lowerCaseLanguage} hideLineNumbers content={exampleCodeSnippet} fullHeight />
             </div>
         </div>
     );
--- src/pages/sdk/index.tsx
+++ src/pages/sdk/index.tsx
@@ -17,7 +17,27 @@ import { PlaywrightCrawler } from 'crawlee';
 
 // Initialize the actor on the platform. This function connects your
 // actor to platform events, storages and API. It replaces Apify.main()
-await Actor.init();`;
+await Actor.init();
+
+const crawler = new PlaywrightCrawler({
+    // handle(Page|Request)Functions of all Crawlers
+    // are now simply called a requestHandler.
+    async requestHandler({ request, page, enqueueLinks }) {
+        const title = await page.title();
+        console.log(\`Title of $\{request.loadedUrl} is '$\{title}'\`);
+        // Use Actor instead of the Apify object to save data.
+        await Actor.pushData({ title, url: request.loadedUrl });
+        // We simplified enqueuing links a lot, see the docs.
+        // This way the function adds only links to same hostname.
+        await enqueueLinks();
+    }
+});
+// You can now add requests to the queue directly from the run function.
+// No need to create an instance of the queue separately.
+await crawler.run(['https://crawlee.dev']);
+// This function disconnects the actor from the platform
+// and optionally sends an exit message.
+await Actor.exit();`;
 
 const pythonExample = `# the Apify SDK makes it easy to read the actor input with the Actor.get_input() method,
 # and to save scraped data from your actors to a dataset by simply using the Actor.push_data() method.
