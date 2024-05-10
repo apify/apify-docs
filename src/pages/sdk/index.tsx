63583f3f8934035c03556dbefc43cc8c85866769
Eva Baldasseroni <37815707+baldasseva@users.noreply.github.com>
2024-05-10 15:23:48 +0200

    docs: Add SDK landing page (#995)
    ### 🎨 What 
    
    Design →
    [desktop](https://www.figma.com/file/r6aPCnTKl1Gy3rPytKgndK/Documentation?type=design&node-id=2859-20664&mode=design&t=DZHUNiUbNnI4xab3-0)
    
    ### 🛠️ How 
    
    - I tried coordinating with PR for API as much as possible ->
    https://github.com/apify/apify-docs/pull/991
    - The design of the background is not respected. Until this PR is closed
    and for a reasonable amount of time, I will try to implement it. It is
    certainly possible, but I don't know how much rewriting of existing or
    writing of new code is necessary.

---
+++ src/pages/sdk/index.tsx
@@ -0,0 +1,109 @@
+import { theme } from '@apify-packages/ui-library';
+import Layout from '@theme/Layout';
+import React from 'react';
+import styled from 'styled-components';
+
+import Hero from '../../components/Hero/Hero';
+import SdkSection from '../../components/SdkSection/SdkSection';
+import Section from '../../components/Section/Section';
+import UiLibraryWrapper from '../../components/UiLibraryWrapper';
+
+const javascriptExample = `// The Apify SDK makes it easy to initialize the actor on the platform with the Actor.init() method,
+// and to save the scraped data from your Actors to a dataset by simply using the Actor.pushData() method.
+
+import { Actor } from 'apify';
+import { PlaywrightCrawler } from 'crawlee';
+
+await Actor.init();
+const crawler = new PlaywrightCrawler({
+    async requestHandler({ request, page, enqueueLinks }) {
+        const title = await page.title();
+        console.log(\`Title of $\{request.loadedUrl} is '$\{title}'\`);
+        await Actor.pushData({ title, url: request.loadedUrl });
+        await enqueueLinks();
+    }
+});
+await crawler.run(['https://crawlee.dev']);
+await Actor.exit();`;
+
+const pythonExample = `# The Apify SDK makes it easy to read the actor input with the Actor.get_input() method,
+# and to save the scraped data from your Actors to a dataset by simply using the Actor.push_data() method.
+
+from apify import Actor
+from bs4 import BeautifulSoup
+import requests
+
+async def main():
+    async with Actor:
+        actor_input = await Actor.get_input()
+        response = requests.get(actor_input['url'])
+        soup = BeautifulSoup(response.content, 'html.parser')
+        await Actor.push_data({ 'url': actor_input['url'], 'title': soup.title.string })`;
+
+const HeroWrapper = styled(Hero)`
+        background-image: url("/img/hero_background.svg");
+    `;
+
+const StyledContent = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.space.space80};
+
+    @media (max-width: ${theme.layout.tablet}) {
+        padding: 0 ${theme.space.space16} ${theme.space.space80} ${theme.space.space16};
+    }
+
+    @media (min-width: ${theme.layout.tablet}) {
+        padding: ${theme.space.space40} ${theme.space.space24} ${theme.space.space80} ${theme.space.space24};
+    }
+
+    @media (min-width: ${theme.layout.largeDesktop}) {
+        max-width: 1200px;
+        padding: ${theme.space.space40} ${theme.space.space40} ${theme.space.space80} ${theme.space.space40};
+    }
+`;
+
+export default function Home() {
+    return (
+        <Layout>
+            <UiLibraryWrapper>
+                <HeroWrapper
+                    heading="Apify SDK"
+                    isCentered
+                    description={
+                        <>
+                            The Apify SDK is a toolkit for building Actors—serverless microservices running (not only) on the Apify platform.
+                            Apify comes with first-class support for JavaScript/TypeScript and Python,
+                            but you can run any containerized code on the Apify platform.
+                        </>
+                    }
+                />
+                <Section>
+                    <StyledContent>
+                        <SdkSection
+                            title="SDK for JavaScript"
+                            description="Toolkit for building Actors—serverless microservices running (not only) on the Apify platform."
+                            installCodeSnippet="npx apify-cli create my-crawler"
+                            exampleCodeSnippet={javascriptExample}
+                            language="JavaScript"
+                            githubRepoUrl="https://github.com/apify/apify-sdk-js"
+                            gettingStartedUrl="https://docs.apify.com/sdk/js/docs/guides/apify-platform"
+                            referenceUrl="https://docs.apify.com/sdk/js/reference"
+                        />
+                        <SdkSection
+                            title="SDK for Python"
+                            description="The Apify SDK for Python is the official library for creating Apify Actors in Python.
+            It provides useful features like actor lifecycle management, local storage emulation, and actor event handling."
+                            installCodeSnippet="apify create my-python-actor"
+                            exampleCodeSnippet={pythonExample}
+                            language="Python"
+                            githubRepoUrl="https://github.com/apify/apify-sdk-python"
+                            gettingStartedUrl="https://docs.apify.com/sdk/python/docs/overview/introduction"
+                            referenceUrl="https://docs.apify.com/sdk/python/reference"
+                        />
+                    </StyledContent>
+                </Section>
+            </UiLibraryWrapper>
+        </Layout>);
+}
