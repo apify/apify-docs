ed5a39bae3f33d198eea1e665da130f8cd09bb2d
Eva Baldasseroni <37815707+baldasseva@users.noreply.github.com>
2024-05-16 12:24:01 +0200

    fix(docs): codeblock correct theme (#1006)
    In page /sdk and /api expecially, now the color of the Codeblock texts
    is working with the color theme of the page
    
    ---------
    
    Co-authored-by: HonzaTuron <jan.turon@apify.com>

--- src/components/UiLibraryWrapper.tsx
+++ src/components/UiLibraryWrapper.tsx
@@ -2,15 +2,21 @@ import { UiDependencyProvider } from '@apify-packages/ui-library';
 import Link from '@docusaurus/Link';
 import { useColorMode } from '@docusaurus/theme-common';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 
 export default function UiLibraryWrapper({ children }: PropsWithChildren) {
+    const [themeIsDark, setThemeIsDark] = useState(true);
+    const isDark = useColorMode().isDarkTheme;
+    useEffect(() => {
+        setThemeIsDark(isDark);
+    }, [isDark]);
+
     return (
         <UiDependencyProvider dependencies={{
             InternalLink: (props) => <Link {...props} />,
             windowLocationHost: useBaseUrl(''),
             isHrefTrusted: () => true,
-            uiTheme: useColorMode().isDarkTheme ? 'DARK' : 'LIGHT',
+            uiTheme: themeIsDark ? 'DARK' : 'LIGHT',
         }}>{children}</UiDependencyProvider>
     );
 }
