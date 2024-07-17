dbc245572a5c13b0acdc1f4e535db433c83bd624
Honza Javorek <mail@honzajavorek.cz>
2024-07-17 12:17:13 +0200

    fix: avoid setting frontmatter to partials, as it brings up Docusaurus warning

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -193,12 +193,12 @@ module.exports = {
         mermaid: true,
         parseFrontMatter: async (params) => {
             const result = await params.defaultParseFrontMatter(params);
-
-            const ogImageURL = new URL('https://apify.com/og-image/docs-article');
-
-            ogImageURL.searchParams.set('title', result.frontMatter.title);
-            result.frontMatter.image ??= ogImageURL.toString();
-
+            const isPartial = filePath.split('/').pop()[0] === '_';
+            if (!isPartial) {
+                const ogImageURL = new URL('https://apify.com/og-image/docs-article');
+                ogImageURL.searchParams.set('title', frontMatter.title);
+                frontMatter.image ??= ogImageURL.toString();
+            }
             return result;
         },
     },
