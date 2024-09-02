ab553e66e6f9f615641a1949f9840ac12adf9c90
Daniel Wébr <webrdaniel@gmail.com>
2024-09-02 15:06:14 +0200

    add scripts in Docusaurus configuration instead of the theme

--- apify-docs-theme/src/theme.js
+++ apify-docs-theme/src/theme.js
@@ -132,21 +132,6 @@ This either means that your Docusaurus setup is misconfigured, or that your GitH
             o.plugins.push(postcssPreset); // allow newest CSS syntax
             return o;
         },
-        injectHtmlTags() {
-            return {
-                headTags: [
-                    process.env.INTERCOM_APP_ID && {
-                        tagName: 'script',
-                        innerHTML: `window.intercomSettings={api_base:"https://api-iam.intercom.io",app_id:"${process.env.INTERCOM_APP_ID}"};`,
-
-                    },
-                    process.env.INTERCOM_APP_ID && {
-                        tagName: 'script',
-                        innerHTML: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/${process.env.INTERCOM_APP_ID}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()`,
-                    },
-                ],
-            };
-        },
     };
 }
 
