9ca3b008b66ab8eb22e0916703cf4a500855f076
Jindřich Bär <jindrichbar@gmail.com>
2024-09-06 10:53:02 +0200

    fix: reserve space for bold active TOC items with `::before`

---
+++ apify-docs-theme/src/theme/TOCItems/index.tsx
@@ -0,0 +1,52 @@
+import { useThemeConfig } from '@docusaurus/theme-common';
+import {
+    useTOCHighlight,
+    useFilteredAndTreeifiedTOC,
+    type TOCHighlightConfig,
+} from '@docusaurus/theme-common/internal';
+import type { Props } from '@theme/TOCItems';
+import TOCItemTree from '@theme/TOCItems/Tree';
+import React, { useMemo } from 'react';
+
+export default function TOCItems({
+    toc,
+    className = 'table-of-contents table-of-contents__left-border',
+    linkClassName = 'table-of-contents__link',
+    linkActiveClassName = undefined,
+    minHeadingLevel: minHeadingLevelOption,
+    maxHeadingLevel: maxHeadingLevelOption,
+    ...props
+}: Props): JSX.Element | null {
+    const themeConfig = useThemeConfig();
+
+    const minHeadingLevel = minHeadingLevelOption ?? themeConfig.tableOfContents.minHeadingLevel;
+    const maxHeadingLevel = maxHeadingLevelOption ?? themeConfig.tableOfContents.maxHeadingLevel;
+
+    const tocTree = useFilteredAndTreeifiedTOC({
+        toc,
+        minHeadingLevel,
+        maxHeadingLevel,
+    });
+
+    const tocHighlightConfig: TOCHighlightConfig | undefined = useMemo(() => {
+        if (linkClassName && linkActiveClassName) {
+            return {
+                linkClassName,
+                linkActiveClassName,
+                minHeadingLevel,
+                maxHeadingLevel,
+            };
+        }
+        return undefined;
+    }, [linkClassName, linkActiveClassName, minHeadingLevel, maxHeadingLevel]);
+    useTOCHighlight(tocHighlightConfig);
+
+    return (
+        <TOCItemTree
+            toc={tocTree}
+            className={className}
+            linkClassName={linkClassName}
+            {...props}
+        />
+    );
+}
