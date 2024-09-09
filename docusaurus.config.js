dec77d8ec3091eb114aa66508c37792096279713
Daniel Wébr <webrdaniel@gmail.com>
2024-09-09 11:49:21 +0200

    add client module with hubspot tracking callback

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,16 @@ module.exports = {
             innerHTML: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/${process.env.INTERCOM_APP_ID}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()`,
             attributes: {},
         },
+        // hubspot tracking page view
+        {
+            tagName: 'script',
+            innerHTML: `
+                var _hsq = window._hsq = window._hsq || [];
+                _hsq.push(['setPath', window.location.pathname]);
+                _hsq.push(['trackPageView']);
+            `,
+            attributes: {},
+        },
     ].filter(Boolean),
 
     onBrokenLinks:
@@ -210,4 +220,5 @@ module.exports = {
             '^/legal/*',
         ],
     },
+    clientModules: ['./clientModule.js'],
 };
