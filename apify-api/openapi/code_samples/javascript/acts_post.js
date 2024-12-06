f9e2ba8cba47486adacfd5c7dd94599be7baf62e
Mihails <m.murasovs@gmail.com>
2024-12-06 16:15:35 +0100

    Feat: auto-generate x-codesamples

--- apify-api/openapi/code_samples/javascript/acts_post.js
+++ apify-api/openapi/code_samples/javascript/acts_post.js
@@ -1,8 +1,6 @@
 import { ApifyClient } from 'apify-client';
 
 const apifyClient = new ApifyClient({ token: 'my-token' });
-const actorCollectionClient = apifyClient.actors();
-
-const myActor = await actorCollectionClient.create({ name: 'my-actor' });
+const myActor = await apifyClient.actors().create({ name: 'my-actor' });
 
 console.log(myActor);
