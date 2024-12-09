b71a5623d6c7c6ef2fb27242f3b3587642f100b6
Mihails <m.murasovs@gmail.com>
2024-12-09 13:17:38 +0100

    feat: add code samples for the Actor versions

---
+++ apify-api/openapi/code_samples/javascript/act_version_delete.js
@@ -0,0 +1,7 @@
+import { ApifyClient } from 'apify-client';
+
+const apifyClient = new ApifyClient({ token: 'my-token' });
+// Replace apify/my-sample-actor with your Actor's ID or technical name
+await apifyClient.actor('apify/my-sample-actor')
+    .version('0.1')
+    .delete();
---
+++ apify-api/openapi/code_samples/javascript/act_version_get.js
@@ -0,0 +1,9 @@
+import { ApifyClient } from 'apify-client';
+
+const apifyClient = new ApifyClient({ token: 'my-token' });
+// Replace apify/my-sample-actor with your Actor's ID or technical name
+const version = await apifyClient.actor('apify/my-sample-actor')
+    .version('0.1')
+    .get();
+
+console.log(version);
