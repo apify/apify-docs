5d0f92a095f72d0e71aa3dfcf0753485037d6bf1
Mihails <m.murasovs@gmail.com>
2024-12-09 11:41:23 +0100

    Add JS samples for Actor object

---
+++ apify-api/openapi/code_samples/javascript/act_get.js
@@ -0,0 +1,7 @@
+import { ApifyClient } from 'apify-client';
+
+const apifyClient = new ApifyClient({ token: 'my-token' });
+// Replace apify/my-sample-actor with your Actor's ID or technical name
+const actor = await apifyClient.actor('apify/my-sample-actor').get();
+
+console.log(actor);
--- apify-api/openapi/code_samples/javascript/acts_post.js
+++ apify-api/openapi/code_samples/javascript/acts_post.js
@@ -1,6 +1,6 @@
 import { ApifyClient } from 'apify-client';
 
 const apifyClient = new ApifyClient({ token: 'my-token' });
-const myActor = await apifyClient.actors().create({ name: 'my-actor' });
+const myActor = await apifyClient.actors().create({ name: 'my-sample-actor' });
 
 console.log(myActor);
