d57d035b485f6c21eb0469f691d1f9356e89676d
Mihails <m.murasovs@gmail.com>
2024-12-11 18:47:59 +0100

    fix: better IDs and narrower examples

--- apify-api/openapi/code_samples/javascript/actorTask_get.js
+++ apify-api/openapi/code_samples/javascript/actorTask_get.js
@@ -1,7 +1,10 @@
 import { ApifyClient } from 'apify-client';
 
-const apifyClient = new ApifyClient({ token: 'my-token' });
-// Replace apify~my-sample-task with your task's ID or technical name
-const task = await apifyClient.task('apify~my-sample-task').get();
+const apifyClient = new ApifyClient({
+    token: '<TOKEN>',
+});
+const task = await apifyClient
+    .task('<TASK ID>')
+    .get();
 
 console.log(task);
--- apify-api/openapi/code_samples/javascript/actorTask_webhooks_get.js
+++ apify-api/openapi/code_samples/javascript/actorTask_webhooks_get.js
@@ -1,8 +1,10 @@
 import { ApifyClient } from 'apify-client';
 
-const apifyClient = new ApifyClient({ token: 'my-token' });
-// Replace apify~my-sample-task with your task's ID or technical name
-const { items } = await apifyClient.task('apify~my-sample-task')
+const apifyClient = new ApifyClient({
+    token: '<TOKEN>',
+});
+const { items } = await apifyClient
+    .task('<TASK ID>')
     .webhooks()
     .list();
 
