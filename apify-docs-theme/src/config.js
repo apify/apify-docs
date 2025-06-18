de08496527a758024b272865eda98e0b8be9d311
František Nesveda <frantisek@apify.com>
2025-06-18 13:31:33 +0200

    Simplify env var parsing

--- apify-docs-theme/src/config.js
+++ apify-docs-theme/src/config.js
@@ -10,10 +10,7 @@ if (process.env.LOCALHOST) {
     absoluteUrl = process.env.APIFY_DOCS_ABSOLUTE_URL;
 }
 
-let noIndex = false;
-if (process.env.NO_INDEX) {
-    noIndex = [true, 'true', 1, '1'].includes(process.env.NO_INDEX);
-}
+const noIndex = ['true', '1'].includes(process.env.NO_INDEX ?? '')
 
 const themeConfig = {
     docs: {
