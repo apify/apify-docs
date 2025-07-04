ecf78187cfae653c51b7f54b529d8c8c0069070c
Tomas Katz <katzino@gmail.com>
2025-07-04 12:21:39 +0200

    fix: button text
    Co-authored-by: Martin Adámek <banan23@gmail.com>

--- apify-docs-theme/src/config.js
+++ apify-docs-theme/src/config.js
@@ -277,7 +277,7 @@ const themeConfig = {
         },
         translations: {
             button: {
-                buttonText: 'Search documentation...',
+                buttonText: 'Search documentation…',
             },
         },
     },
