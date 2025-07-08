53c9f8fd22152e36a9f871497b23fab310072f1f
Tomas Katz <katzino@gmail.com>
2025-07-08 10:17:55 +0200

    feat: header facelift (#1662)

--- apify-docs-theme/src/config.js
+++ apify-docs-theme/src/config.js
@@ -138,13 +138,6 @@ const themeConfig = {
                 ],
             },
             {
-                href: 'https://github.com/apify',
-                label: 'GitHub',
-                title: 'Apify on GitHub',
-                position: 'right',
-                className: 'icon',
-            },
-            {
                 href: 'https://discord.com/invite/jyEM2PRvMU',
                 label: 'Discord',
                 title: 'Chat on Discord',
@@ -257,6 +250,10 @@ const themeConfig = {
                         href: 'https://github.com/apify',
                     },
                     {
+                        href: 'https://discord.com/invite/jyEM2PRvMU',
+                        label: 'Discord',
+                    },
+                    {
                         label: 'Trust Center',
                         href: 'https://trust.apify.com',
                     },
@@ -274,8 +271,14 @@ const themeConfig = {
         appId: 'N8EOCSBQGH',
         apiKey: 'e97714a64e2b4b8b8fe0b01cd8592870', // search only (public) API key
         indexName: 'apify_sdk_v2',
+        placeholder: 'Search documentation',
         algoliaOptions: {
             facetFilters: ['version:VERSION'],
+        },
+        translations: {
+            button: {
+                buttonText: 'Search documentation…',
+            },
         },
     },
     hubspot: {
---
+++ apify-docs-theme/src/theme/Navbar/CTA/index.jsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+export default function NavbarCTA() {
+    return (
+        <a href="https://console.apify.com" className={styles.getStarted}>Get started</a>
+    );
+}
