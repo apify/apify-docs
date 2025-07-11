92aa96dcff85ce3f26b2fd55b8982c0b82091d27
Tomas Katz <katzino@gmail.com>
2025-07-11 13:08:20 +0200

    fix: analytics tracking [internal] (#1684)

--- apify-docs-theme/src/theme/Navbar/CTA/index.jsx
+++ apify-docs-theme/src/theme/Navbar/CTA/index.jsx
@@ -1,7 +1,20 @@
 import styles from './styles.module.css';
 
+const buttonText = 'Get started';
+
+function clickHandler() {
+    if (window.analytics) {
+        // Track the click event
+        window.analytics.track('Clicked', {
+            app: 'docs',
+            button_text: buttonText,
+            element: 'header.signUpButton',
+        });
+    }
+}
+
 export default function NavbarCTA() {
     return (
-        <a href="https://console.apify.com" className={styles.getStarted}>Get started</a>
+        <a href="https://console.apify.com" onClick={clickHandler} className={styles.getStarted}>{buttonText}</a>
     );
 }
