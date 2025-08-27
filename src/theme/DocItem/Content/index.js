e320b74746b36e40e3616dc7b412d71e0dbfe50f
patrikbraborec <patrikbraborec@gmail.com>
2025-08-27 09:19:33 +0200

    Add debug for LLM buttons

--- src/theme/DocItem/Content/index.js
+++ src/theme/DocItem/Content/index.js
@@ -53,6 +53,8 @@ export default function DocItemContent({ children }) {
 
   const shouldShowLLMButtons = allowedPaths.some((path) => location.pathname.startsWith(path));
 
+  console.log('debug: shouldShowLLMButtons', shouldShowLLMButtons);
+
   return (
     <div className={clsx(ThemeClassNames.docs.docMarkdown, 'markdown')}>
       {syntheticTitle && <Heading as="h1">{syntheticTitle}</Heading>}
