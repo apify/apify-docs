60ab78645bcd4c829adda0e392d4e9c7b7b5ec7a
Jindřich Bär <jindrichbar@gmail.com>
2025-09-18 15:56:19 +0200

    feat: enable Kapa AI chat widget

--- apify-docs-theme/src/config.js
+++ apify-docs-theme/src/config.js
@@ -322,8 +322,6 @@ const scripts = [
         'data-modal-title': 'Apify AI Assistant',
         'data-project-color': '#666666',
         'data-button-hide': 'true',
-        'data-search-mode-enabled': 'true',
-        'data-search-include-source-names': '["Docs"]',
         'data-project-logo': 'https://apify.com/img/apify-logo/logomark-32x32.svg',
         'data-modal-example-questions': 'How to run an Actor?,Create a version of an Actor?',
         'data-modal-override-open-id': 'ask-ai-input',
