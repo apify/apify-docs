c7ee3f8d83c84ea9282212d45dd98596bb9881b1
Jan Curn <jan.curn@gmail.com>
2025-09-18 12:55:03 +0200

    Better copy

--- sources/legal/sidebars.js
+++ sources/legal/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
     legal: [
         {
             type: 'link',
-            label: 'Legal info (Impressum)',
+            label: 'Legal info (Imprint)',
             href: '/legal',
         },
         {
