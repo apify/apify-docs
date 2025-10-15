393f217ee80cc785e65cda2d73de3f431312c156
katzino <katzino@gmail.com>
2025-10-15 11:53:57 +0200

    chore: defer `window.location`

--- src/plugins/docusaurus-plugin-segment/segment.js
+++ src/plugins/docusaurus-plugin-segment/segment.js
@@ -30,16 +30,19 @@ function getOneTrustConsentContext() {
 
 export default ExecutionEnvironment.canUseDOM ? {
     onRouteUpdate() {
-        // Don't track page views on development
-        if (process.env.NODE_ENV === 'production' && window.analytics) {
-            window.analytics.page({
-                app: 'docs',
-                page_type: 'DOCS_PAGE',
-                path: window.location.pathname,
-                url: window.location.href,
-                search: window.location.search,
-                ...getOneTrustConsentContext(),
-            });
-        }
+        // this forces deferred execution that ensures `window.location` is in sync
+        setTimeout(() => {
+            // Don't track page views on development
+            if (process.env.NODE_ENV === 'production' && window.analytics) {
+                window.analytics.page({
+                    app: 'docs',
+                    page_type: 'DOCS_PAGE',
+                    path: window.location.pathname,
+                    url: window.location.href,
+                    search: window.location.search,
+                    ...getOneTrustConsentContext(),
+                });
+            }
+        }, 0);
     },
 } : null;
