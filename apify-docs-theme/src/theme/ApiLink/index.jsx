d3230fddfdb1608fdd45b357fc017cb3528a4626
Jindřich Bär <jindrichbar@gmail.com>
2026-05-14 15:33:02 +0200

    chore: address PR comments

--- apify-docs-theme/src/theme/ApiLink/index.jsx
+++ apify-docs-theme/src/theme/ApiLink/index.jsx
@@ -1,16 +1,14 @@
 import Link from '@docusaurus/Link';
 import { useDocsVersion } from '@docusaurus/plugin-content-docs/client';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { usePluginData } from '@docusaurus/useGlobalData';
 import React from 'react';
 
 export default function ApiLink({ to, children }) {
     const { version, isLast } = useDocsVersion();
-    const { siteConfig } = useDocusaurusContext();
     const typedocData = usePluginData('docusaurus-plugin-typedoc-api');
     const routeBasePath = (typedocData?.routeBasePath ?? 'api').replace(/^\/+|\/+$/g, '');
 
-    if (siteConfig.presets[0][1].docs.disableVersioning || isLast) {
+    if (isLast) {
         return <Link to={`/${routeBasePath}/${to}`}>{children}</Link>;
     }
 
