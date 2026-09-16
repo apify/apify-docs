aecda0d1cf2eab933d17949e3eaff30eb43ee848
Honza Javorek <mail@honzajavorek.cz>
2026-09-16 10:08:40 +0200

    change default title and use it

--- src/components/PromptButton.jsx
+++ src/components/PromptButton.jsx
@@ -4,7 +4,7 @@ import styles from './PromptButton.module.css';
 
 export default function PromptButton({
     prompt,
-    title = 'Use pre-built prompt to get started faster.',
+    title = 'Use pre-built prompt for your AI assistant',
 }) {
     const [copied, setCopied] = useState(false);
     const [showPrompt, setShowPrompt] = useState(false);
